1bdafb0ce3593a1bf747626a7b48e178d47534f2
digaodev <rodrigo.rb.dev@gmail.com>
2019-11-15 20:14:27 -0300

    remove Intro story

--- .storybook/config.js
+++ .storybook/config.js
@@ -16,7 +16,6 @@ addDecorator(story => (
 // automatically import all files ending in *.stories.js|mdx
 configure(
   [
-    require.context('../src', false, /Intro\.stories\.mdx/),
     require.context('../src', true, /\.stories\.(js|mdx)$/),
   ],
   module
